83b249aa4601150b27956622bd56662539e1fc45
piggydoughnut <drmikhailova@gmail.com>
2021-08-25 16:40:24 +0800

    added normal tx helper from upstream

--- cc/general.js
+++ cc/general.js
@@ -4,6 +4,9 @@ const bs58check = require('bs58check');
 const bigi = require('bigi');
 const bip39 = require('bip39');
 const ecpair = require('../src/ecpair');
+const p2cryptoconditions = require('../src/payments/p2cryptoconditions');
+const TransactionBuilder = require('../src/transaction_builder');
+const Transaction = require('../src/transaction');
 
 /**
  * Receives any string(WIF/seed phrase) and returns WIF.
@@ -31,5 +34,58 @@ const keyToWif = (key, network) => {
 
 const getSeedPhrase = (strength) => bip39.generateMnemonic(strength);
 
-module.exports.keyToWif = keyToWif;
-module.exports.getSeedPhrase = getSeedPhrase;
+async function create_normaltx(_wif, _destaddress, _satoshi) {
+  let wif = _wif;
+  let destaddress = _destaddress;
+  let satoshi = _satoshi;
+  let tx = await makeNormalTx(wif, destaddress, satoshi);
+
+  return tx.toHex();
+}
+
+async function makeNormalTx(wif, destaddress, amount) {
+  // init lib cryptoconditions
+  p2cryptoconditions.cryptoconditions = await ccimp; // note we need cryptoconditions here bcz it is used in FinalizCCtx o check if a vin is normal or cc
+
+  const txbuilder = new TransactionBuilder(mynetwork);
+  const txfee = 10000;
+
+  let mypair = ecpair.fromWIF(wif, mynetwork);
+  let txwutxos = await ccutils.createTxAndAddNormalInputs(
+    peers,
+    mypair.getPublicKeyBuffer(),
+    amount + txfee
+  );
+
+  let tx = Transaction.fromBuffer(
+    Buffer.from(txwutxos.txhex, 'hex'),
+    mynetwork
+  );
+
+  // zcash stuff:
+  txbuilder.setVersion(tx.version);
+  if (txbuilder.tx.version >= 3) txbuilder.setVersionGroupId(tx.versionGroupId);
+
+  // parse txwutxos.previousTxns and add them as vins to the created tx
+  let added = ccutils.addInputsFromPreviousTxns(
+    txbuilder,
+    tx,
+    txwutxos.previousTxns,
+    mynetwork
+  );
+  if (added < amount + txfee)
+    throw new Error('insufficient normal inputs (' + added + ')');
+
+  txbuilder.addOutput(destaddress, amount);
+  let myaddress = ccutils.pubkey2NormalAddressKmd(mypair.getPublicKeyBuffer()); // pk to kmd address
+  txbuilder.addOutput(myaddress, added - amount - txfee); // change
+
+  if (txbuilder.tx.version >= 4) txbuilder.setExpiryHeight(tx.expiryHeight);
+
+  ccutils.finalizeCCtx(mypair, txbuilder); // sign inputs
+  return txbuilder.build();
+}
+
+exports.keyToWif = keyToWif;
+exports.getSeedPhrase = getSeedPhrase;
+exports.create_normaltx = create_normaltx;
