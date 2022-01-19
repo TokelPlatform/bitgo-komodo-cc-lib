58d311ce98a3d75a4112e0ca334d5b0f98859c81
dimxy <dimxy@komodoplatform.com>
2022-01-19 13:45:50 +0500

    enable notarisation validation code (turned off for nodes upgrade period)

--- net/nspvPeer.js
+++ net/nspvPeer.js
@@ -7,7 +7,7 @@ const logerror = Debug('nspv:error');
 const old = require('old')
 const bufferutils = require("../src/bufferutils");
 const Peer = require('./peer')
-const { NSPVMSGS, NSPV_VERSION, NSPV_VERSION_5, nspvReq, nspvResp, nspvMsgName } = require('./kmdtypes');
+const { NSPVMSGS, NSPV_VERSION, nspvReq, nspvResp, nspvMsgName } = require('./kmdtypes');
 const { hashFromHex, hashToHex, isValidHash, castHashBin } = require('../cc/ccutils');
 const typeforce = require('typeforce');
 const types = require('../src/types');
@@ -50,14 +50,14 @@ Peer.prototype._registerListeners = function() {
     logdebug("on 'verack' event fired")
     // after verack received we must send NSPV_INFO (sort of secondary nspv connect) to check versions
     this.nspvGetInfo(0, {}, (err, nspvInfo, peer) => {
-      if (nspvInfo?.version === NSPV_VERSION || nspvInfo?.version === NSPV_VERSION_5 /*TODO: temp allow old nodes*/)  {
+      if (nspvInfo?.version === NSPV_VERSION)  {
         this.gotNspvInfo = true;
         this.nspvVersion = nspvInfo.version;
         this._nspvReady();
       } else {
         if (err)
           logerror(err?.message, peer.getUrl());
-        if (nspvInfo && (nspvInfo.version !== NSPV_VERSION || nspvInfo.version !== NSPV_VERSION_5))
+        if (nspvInfo && (nspvInfo.version !== NSPV_VERSION))
           logerror('unsupported remote nspv node version', nspvInfo?.version, peer.getUrl());
       }
     });
@@ -119,12 +119,6 @@ Peer.prototype.nspvGetInfo = function(reqHeight, opts, cb, expectVersion) {
   var onNspvResp = (resp) => {
     if (timeout) clearTimeout(timeout)
     if (resp && resp?.respCode === NSPVMSGS.NSPV_ERRORRESP) {
-      // TODO: temp try to reconnect as version 5
-      if (resp.errCode == -13 && !expectVersion)  { // 'version not supported' && we have not tried older version yet 
-        this.nspvGetInfo(reqHeight, opts, cb, NSPV_VERSION_5);
-        return;
-      }
-
       let error = new NspvError("nspv remote error: " + resp?.errDesc, NSPVMSGS.NSPV_INFO, 11);
       cb(error, null, this);
       this._error(error);  // disconnect peer if getinfo could not be done (it is like nspv verack)
