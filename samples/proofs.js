2fd80252f05237eba227e636efbbc94744d302ac
dimxy <dimxy@komodoplatform.com>
2022-01-28 23:03:38 +0500

    unused code removed; tests reordered

--- samples/proofs.js
+++ samples/proofs.js
@@ -1,21 +1,8 @@
 
 'use strict';
 
-const TransactionBuilder = require('../src/transaction_builder');
-const Transaction = require('../src/transaction');
-const ECPair = require('../src/ecpair');
-const OPS = require('bitcoin-ops');
-
-const bufferutils = require("../src/bufferutils");
-const script = require("../src/script");
-const kmdmessages = require('../net/kmdmessages');
-const ccutils = require('../cc/ccutils');
-const ecpair = require('../src/ecpair');
 
 const ntzsproofs = require('../cc/ntzproofs');
-const kmdblockindex = require('../src/kmdblockindex');
-
-var bmp = require('bitcoin-merkle-proof')
 const connect = require('../net/connect.js')
 
 
@@ -31,46 +18,18 @@ const networks = require('../src/networks');
 const mynetwork = networks.TOKEL; 
 
 
-/*
-to connect over p2p:
-var dnsSeeds = [
-]
-*/
-
-// to connect over p2p
-var staticPeers = [
-    //'18.189.25.123:14722'
-    //'rick.kmd.dev:25434'
-    '127.0.0.1:22024'
-] 
-
-
-// to connect over websockets:
-var webSeeds = [
-    //'ws://18.189.25.123:8192'
-    //'ws://localhost:8192'
-    'ws://3.136.47.223:8192'
-    // TODO: add more
-]
-
 var params = {
     network: mynetwork,
-    //defaultPort: 8192,
-    //dnsSeeds: dnsSeeds,
-    //webSeeds: webSeeds,
-    //staticPeers: staticPeers,  // dnsSeed works also
-    //protocolVersion: 170009,
-    //messages: kmdmessages.kmdMessages
 }
 
 var opts = {
-    //connectWeb: true,     // use websockets
-    //wrtc: wrtc,          // not supported any more
     numPeers: 8,
     //hardLimit: 2,        // max peers
-    //connectPlainWeb: true,  // use plain websockets, no PXP
-    wsOpts: { rejectUnauthorized: false } 
+    //connectPlainWeb: true,  // use plain websockets
+    // wsOpts: { rejectUnauthorized: false } 
 }
+
+
 
 if (!process.browser) 
 {
@@ -79,29 +38,36 @@ if (!process.browser)
 
     try {
 
+      let sleep = function(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      // wait for at least 2 peers
+      /*while (peers.peers.length < 2)  {  
+        await sleep(2000);
+        console.log('peers.peers.length=', peers.peers.length);
+      }*/
+
       // tests:
       
+      /*
       // get txproof for txid:
-      //let txid = 'fcaf0d4ca6c7392fe67474738da9f51acacd74bd31ae29260085ec9254020768'  // tokel h=10000
-      //let ht = 10000;
+      let txid = 'fcaf0d4ca6c7392fe67474738da9f51acacd74bd31ae29260085ec9254020768'  // tokel h=10000
+      let ht = 10000;
 
       //let txid = '118a95dd6aa92bedc13f223ad5f51a6d6c113313b0f2cc16107e2cac0ccf643c' // DIMXY24
       //let txid = 'cce11829d3589cb930ededbf6c0da5cd6d38ac860717308d345f151e7666b54a' //tkltest
       
-      /*
       let txproofresp = await ntzsproofs.nspvTxProof(peers, txid, 0, 0); //tokel
       console.log('txproofresp=', txproofresp); 
       let hashes = bmp.verify(txproofresp.partialMerkleTree);
       console.log('verify result compare txids =', hashes.length > 0 ? Buffer.compare(hashes[0], ccutils.hashFromHex(txid)) == 0 : null );
       */
 
-
       //let ntzresp = await ntzsproofs.nspvNtzs(peers, 10000);
       //console.log('ntzresp=', ntzresp);
 
       //let ntzsproofresp = await ntzsproofs.nspvNtzsProof(peers, ntzresp.ntz.txid);
       //console.log('ntzsproofresp=', ntzsproofresp);
-      
       
       let txid1 = '22eca5965bc69361183653aa69fdcdc4f90a3b4a7b39c96e36d042478ff54e34'; 
       let ht1 = 120000;
@@ -165,7 +131,6 @@ if (!process.browser)
     
       //let txproofvalid = await ntzsproofs.validateTxUsingTxProof(peers, txid);
       //console.log("txproof valid=", txproofvalid);
-      
 
       /* download headers
       let locnew = Buffer.from("0c750b86967a5873b3c3f4ba46f1188b731a82327799aa888598582aa61f654b", 'hex');
